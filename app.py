02b5b4ff9af212314407145c3a7a3068ce289469
DrMadhushan <dev.drmad@gmail.com>
2023-11-22 19:25:43 +0530

    integrate functions and render output with dict data

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, jsonify, make_response, render_template, request 
+import preprocessor as pp
+import predictor
+
 app = Flask(__name__) 
 
 @app.route('/') 
@@ -13,9 +16,15 @@ def upload():
     image = request.files['image']
 
     # You can customize the destination folder and filename as needed
-    image.save('uploads/' + image.filename)
+    image_path = 'uploads/' + image.filename
+    image.save(image_path)
 
-    return jsonify({'message': 'Image uploaded successfully', 'filename': image.filename})
+    input_image = pp.process_image(image_path=image_path)
 
-if __name__ == '__main__': 
+    emotion_weights = predictor.predict_emotion(image=input_image)
+
+    return render_template('output.html', emotion_weights=emotion_weights)
+    # return jsonify({'message': 'Image uploaded successfully', 'filename': image.filename})
+
+if __name__ == '__main__':
    app.run(debug = True)
