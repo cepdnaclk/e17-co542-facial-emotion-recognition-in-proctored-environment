7b719021533c12393e77dfd61627ecdcdde8a647
Arshad-MRM <e17015@eng.pdn.ac.lk>
2023-11-23 00:18:13 +0530

    updating working code

--- app.py
+++ app.py
@@ -10,21 +10,22 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    print(request.files)
     if 'image' not in request.files:
         return jsonify({'error': 'No image provided'}), 400
 
     image = request.files['image']
+    
 
     # You can customize the destination folder and filename as needed
-    image_path = 'uploads/' + image.filename
+    image_path = 'static/'+image.filename
     image.save(image_path)
-
+    print(image_path)
     input_image = pp.process_image(image_path=image_path)
 
     emotion_weights = predictor.predict_emotion(image=input_image)
-
-    return render_template('output.html', emotion_weights=emotion_weights)
-    # return jsonify({'message': 'Image uploaded successfully', 'filename': image.filename})
+    print(emotion_weights)
+    return render_template('output.html', emotion_weights=emotion_weights, filename=image.filename)
 
 if __name__ == '__main__':
    app.run(debug = True)
